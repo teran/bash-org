2e14339f83e225d26df03ac4e42beeb6f762421c
Igor Shishkin <me@teran.ru>
2016-03-26 01:09:58 +0300

    Initial Commit

---
+++ sampleapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleapp
+//
+//  Created by Igor Shishkin on 26/03/16.
+//  Copyright © 2016 Igor Shishkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
