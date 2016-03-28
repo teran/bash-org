f0c60fe9f6ddddf19c9b2bae321be28a779a2fd5
Igor Shishkin <me@teran.ru>
2016-03-28 04:04:11 +0300

    Behavior changed to use random quotes + Share button added
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- sampleapp/ViewController.swift
+++ sampleapp/ViewController.swift
@@ -7,41 +7,30 @@
 //
 
 import UIKit
+import Social
 
-class ViewController: UIViewController, NSXMLParserDelegate {
-    var strXMLData:String = ""
-    var currentElement:String = ""
-    var passData:Bool=false
-    var passName:Bool=false
-    var parser = NSXMLParser()
-    
-    // var refreshControl = UIKit.UIRefreshControl()
-    
+import Just
+import Kanna
+
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        NSLog("Initialized")
         refreshData()
         mainTextView.textContainer.lineBreakMode = NSLineBreakMode.ByCharWrapping
-        // refreshControl.attributedTitle = NSAttributedString(string:"refreshing...")
-        // refreshControl.addTarget(self, action: "refreshData", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     func refreshData() {
-        let url:String="http://bash.im/rss/"
-        NSLog("Setting up NSURL object")
-        let urlToSend: NSURL = NSURL(string: url)!
-        // Parse the XML
-        NSLog("Setting up NSXMLParser object")
-        parser = NSXMLParser(contentsOfURL: urlToSend)!
-        NSLog("Parser delegation")
-        parser.delegate = self
-        let success:Bool = parser.parse()
-        
-        if(success) {
-            NSLog(strXMLData)
+        let request = Just.get("http://bash.im/random")
+        if request.ok {
+            let text = String(data:request.content!, encoding: NSWindowsCP1251StringEncoding)
+            let doc = Kanna.HTML(html: text!, encoding: NSWindowsCP1251StringEncoding)
+            var quote = doc!.css("div .text")[0].innerHTML
+            quote = quote?.stringByReplacingOccurrencesOfString("<br>", withString: "\n")
+            quote = quote?.stringByReplacingOccurrencesOfString("&lt;", withString: "<")
+            quote = quote?.stringByReplacingOccurrencesOfString("&gt;", withString: ">")
+            mainTextView.text = quote
         } else {
-            NSLog("Error parsing XML data")
+            NSLog("Error performing HTTP request: code \(request.statusCode)")
         }
     }
     
@@ -50,46 +39,16 @@ class ViewController: UIViewController, NSXMLParserDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
-        currentElement=elementName;
-        if(elementName=="description")
-        {
-            if(elementName=="name"){
-                passName=true;
-            }
-            passData=true;
-        }
-    }
-    
-    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        currentElement="";
-        if(elementName=="description")
-        {
-            if(elementName=="name"){
-                passName=false;
-            }
-            passData=false;
-        }
-    }
-    
-    func parser(parser: NSXMLParser, foundCharacters string: String) {
-        if(passName){
-            strXMLData=strXMLData+"\n\n"+string
-        }
-        
-        if(passData) {
-            mainTextView.text = String(string.stringByReplacingOccurrencesOfString("<br>", withString: "\n", options: NSStringCompareOptions.LiteralSearch, range: nil))
-        }
-    }
-    
-    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
-        NSLog("failure error: %@", parseError)
-    }
-    
     @IBOutlet weak var mainTextView: UITextView!
 
     @IBAction func buttonPressed(sender: UIButton) {
         refreshData()
+        
+    }
+    @IBAction func shareButtonPressed(sender: AnyObject) {
+        let tweetComposer = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        tweetComposer.setInitialText(mainTextView.text)
+        self.presentViewController(tweetComposer, animated: true, completion: nil)
     }
 }
 
